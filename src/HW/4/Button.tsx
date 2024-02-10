8c397713ece957be40d9b934ee4ae52980db3b4a
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-10 21:02:20 +0300

    Initial commit

---
+++ src/HW/4/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonPropsType = {
+  callBack: ()=> void
+  name: string
+};
+
+export const Button = ({callBack, name}: ButtonPropsType) => {
+  const callBackHandler = () => {
+    callBack();
+  };
+
+  return (
+    <button id={'hw04-button'} onClick={callBackHandler}>
+      {name}
+    </button>
+  );
+};
